960066f2c0a3d894cacdc3812499fab561c92b3e
wasuken <wevorence@gmail.com>
2019-03-28 21:11:02 +0900

    added user.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,16 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create, :new]
+
   def index
+    @questions = Question.where('user_id = ?', current_user.id)
+    render 'questions/index'
   end
 
   def create
+    render 'questions/create'
   end
 
   def new
+    render 'questions/new'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'answers/index', to: ''
+  devise_for :users
+  get 'answers/index'
   get 'answers/create'
   get 'answers/new'
   get 'questions/index'
   get 'questions/create'
   get 'questions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'questions#index'
 end
