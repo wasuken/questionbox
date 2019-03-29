0c8d7d8d3b1712899ea25a01f79f110431fb4ec0
wasuken <wevorence@gmail.com>
2019-03-29 12:57:26 +0900

    commit.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,10 +7,15 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @qp = params['question']
+    Question.create(question: qp['question'], user_id: qp['user_id'])
     render 'questions/create'
   end
 
   def new
+    @user_id_list = @User.select(:id).where('id <> ?', current_user.id)
+                      .map{|id| [User.find(id).email, id]}
+    @question = Question.new
     render 'questions/new'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'answers/index'
-  get 'answers/create'
+  post 'answers/create'
   get 'answers/new'
   get 'questions/index'
-  get 'questions/create'
+  post 'questions/create'
   get 'questions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'questions#index'
